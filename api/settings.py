6a2899f0d1831ed55a13c07badc2f6a916cef088
Emerdinger <servicesemer@gmail.com>
2021-11-08 14:20:05 -0500

    first commit

--- api/settings.py
+++ api/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
-    'RecomendApp.apps.RecomendappConfig'
+    'RecomendApp.apps.RecomendappConfig',
+    'django-heroku'
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
